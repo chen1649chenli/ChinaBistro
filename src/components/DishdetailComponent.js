00f2fe17a7620a02ef5d104a106f54643615e6a6
Li Chen <chen1649chenli@gmail.com>
2019-06-22 07:36:36 -0400

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,7 +9,7 @@ const maxLength = (len) => (val) => !(val) || val.length <= len;
 const minLength = (len) => (val) => val && (val.length >= len);
 
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, dishId}){
     if (comments != null){
         return (
             <div className="col-12 col-md-5 m-1">
@@ -27,7 +27,7 @@ function RenderComments({comments}){
                     );
                 })}
                 </ul>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment}/>
             </div>
             
 
@@ -70,8 +70,9 @@ export class CommentForm extends Component {
 
     handleSubmit(values){
         this.toggleModal();
-        console.log("Current State is: "+JSON.stringify(values));
-        alert("Current State is: "+JSON.stringify(values));
+        this.props.addComment(this.props.dishId, 
+            values.rating, values.author, values.comment);
+
     }
 
     render(){
@@ -154,7 +155,9 @@ const DishDetail = (props) => {
                 </div>
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments} 
+                    addComment={props.addComment}
+                    dishId={props.dish.id} />
                 </div>
             </div>
         );
